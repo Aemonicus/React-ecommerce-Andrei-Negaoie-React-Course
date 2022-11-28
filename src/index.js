ef1802129185811709c64472d570f3b514b27abc
Florent VICENTE <vicentegroupe@gmail.com>
2022-11-28 14:30:35 +0100

    redux persist local storage

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import { BrowserRouter } from "react-router-dom"
 import App from './App';
 import { Provider } from 'react-redux';
 import './index.scss';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
